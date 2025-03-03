65577894d54e27afa2220f3c15098f25933da2fb
Jae Yun Park <wodbs720@naver.com>
2025-03-03 10:24:35 +0900

    init commit

---
+++ src/main/java/com/jyp/service/my_today_service/MyTodayServiceApplication.java
@@ -0,0 +1,13 @@
+package com.jyp.service.my_today_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTodayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTodayServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jyp/service/my_today_service/MyTodayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jyp.service.my_today_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTodayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
