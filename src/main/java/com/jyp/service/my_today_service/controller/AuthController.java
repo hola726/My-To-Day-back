c424c427656954ccc4c3ef736fec24339e216ba1
Jae Yun Park <wodbs720@naver.com>
2025-03-14 07:56:18 +0900

    feat: 로그인 dto 생성

--- src/main/java/com/jyp/service/my_today_service/controller/AuthController.java
+++ src/main/java/com/jyp/service/my_today_service/controller/AuthController.java
@@ -8,23 +8,17 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.HashMap;
-import java.util.Map;
 
 
 @Tag(name = "인증 API")
 @RestController
 public class AuthController {
     private UserService userService;
+
 
     @Autowired
     public AuthController(UserService userService) {
--- src/main/java/com/jyp/service/my_today_service/dto/SignInDto.java
+++ src/main/java/com/jyp/service/my_today_service/dto/SignInDto.java
@@ -1,12 +1,11 @@
 package com.jyp.service.my_today_service.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
-// 사용자 회원가입 dto
-public class SignUpDto {
+// 사용자 로그인 dto
+public class SignInDto {
     @Schema(description = "사용자 ID")
     @NotEmpty(message = "사용자 ID는 필수 입니다.")
     @Size(max = 16, message = "사용자 ID의 길이는 최대 16자리 입니다.")
@@ -15,15 +14,13 @@ public class SignUpDto {
     @NotEmpty(message = "사용자 비밀번호는 필수 입니다.")
     @Size(min = 8, max = 13, message = "사용자 비밀번호는 최소 8자리 최대 13자리 입니다.")
     private String password;
-    @Schema(description = "사용자 이메일")
-    @NotEmpty(message = "사용자 이메일은 필수 입니다.")
-    @Email(message = "유효한 이메일 형식이여야 합니다.")
-    private String email;
 
-    public SignUpDto(String userId, String password, String email) {
+    public SignInDto() {
+    }
+
+    public SignInDto(String userId, String password) {
         this.userId = userId;
         this.password = password;
-        this.email = email;
     }
 
     public String getUserId() {
@@ -34,19 +31,11 @@ public class SignUpDto {
         return password;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
     public void setUserId(String userId) {
         this.userId = userId;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
