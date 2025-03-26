bff4fb5a2f78e3576fce02a2a86b72a0cd58c7b0
Jae Yun Park <wodbs720@naver.com>
2025-03-27 06:20:07 +0900

    feat: 로그아웃 api 생성

--- src/main/java/com/jyp/service/my_today_service/controller/AuthController.java
+++ src/main/java/com/jyp/service/my_today_service/controller/AuthController.java
@@ -3,27 +3,33 @@ package com.jyp.service.my_today_service.controller;
 import com.jyp.service.my_today_service.dto.ApiResponse;
 import com.jyp.service.my_today_service.dto.SignUpDto;
 import com.jyp.service.my_today_service.dto.UserInfoDto;
+import com.jyp.service.my_today_service.model.BlackListToken;
 import com.jyp.service.my_today_service.model.Users;
+import com.jyp.service.my_today_service.service.BlackListTokenService;
 import com.jyp.service.my_today_service.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
 
 
 @Tag(name = "인증 API")
 @RestController
 public class AuthController {
-    private UserService userService;
+    private final UserService userService;
+    private final BlackListTokenService blackListTokenService;
 
 
     @Autowired
-    public AuthController(UserService userService) {
+    public AuthController(UserService userService, BlackListTokenService blackListTokenService) {
         this.userService = userService;
+        this.blackListTokenService = blackListTokenService;
     }
 
     @Operation(summary = "회원가입 API")
@@ -33,5 +39,19 @@ public class AuthController {
         final UserInfoDto userInfo = new UserInfoDto(user.getUserId(), user.getEmail());
         ApiResponse<UserInfoDto> response = new ApiResponse<>(true, "", "", userInfo);
         return ResponseEntity.ok(response);
+    }
+
+    @Operation(summary = "로그아웃 API")
+    @PostMapping("v1.0/sign-out")
+    public ResponseEntity<ApiResponse<String>> SignOut(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if (header != null && header.startsWith("Bearer ")) {
+            String token = header.substring(7);
+            LocalDateTime expiredDate = LocalDateTime.now();
+            blackListTokenService.setBlackListTokenRepository(token, expiredDate);
+            return ResponseEntity.ok(new ApiResponse<String>(true, "", "", token));
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new ApiResponse<String>(true, "", "", "{\"error\": \"유효한 토큰 필요\"}"));
     }
 }
