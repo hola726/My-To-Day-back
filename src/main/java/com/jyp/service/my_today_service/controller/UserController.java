b92579820cd9223752ce5bdf3f41ae592db665c1
Jae Yun Park <wodbs720@naver.com>
2025-04-02 06:18:26 +0900

    feat: user api userId 쿼리파람으로 안받아도 되게 변경

--- src/main/java/com/jyp/service/my_today_service/controller/UserController.java
+++ src/main/java/com/jyp/service/my_today_service/controller/UserController.java
@@ -10,59 +10,47 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
 
 @Tag(name = "유저 API")
 @RestController
 public class UserController {
     private final UserService userService;
+    private final AuthUtil authUtil;
 
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, AuthUtil authUtil) {
         this.userService = userService;
+        this.authUtil = authUtil;
     }
 
-    @Operation(summary = "사용자 조회(사용자 ID)")
-    @GetMapping("/v1.0/user/{userId}")
-    public ResponseEntity<ApiResponse<UserInfoDto>> getUserByUserId(@PathVariable String userId) {
-        final Optional<Users> user = userService.findUserByUserId(userId);
-        if (user.isEmpty()) {
-            ApiResponse<UserInfoDto> errorResponse = new ApiResponse<>(false, "USER_NOT_FOUND", "User with ID " + userId + " not found", null);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse); // 404
-        }
-        final UserInfoDto userInfoDto = new UserInfoDto(user.get().getUserId(), user.get().getEmail());
+    @Operation(summary = "사용자 조회")
+    @GetMapping("/v1.0/user")
+    public ResponseEntity<ApiResponse<UserInfoDto>> getUserByUserId() {
+        final Users user = authUtil.getUser();
+        final UserInfoDto userInfoDto = new UserInfoDto(user.getUserId(), user.getEmail());
         ApiResponse<UserInfoDto> response = new ApiResponse<>(true, "", "", userInfoDto);
         return ResponseEntity.ok(response);
     }
 
     @Operation(summary = "사용자 삭제")
-    @DeleteMapping("/v1.0/user/{id}")
-    public ResponseEntity<ApiResponse<Boolean>> deleteUser(@PathVariable Long id) {
-        final Optional<Users> user = userService.findUserById(id);
-        if (user.isEmpty()) {
-            ApiResponse<Boolean> errorResponse = new ApiResponse<>(false, "USER_NOT_FOUND", "UserId " + id + " not found", false);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
-        }
-        userService.deleteUserById(id);
+    @DeleteMapping("/v1.0/user")
+    public ResponseEntity<ApiResponse<Boolean>> deleteUser() {
+        final Users user = authUtil.getUser();
+        userService.deleteUserById(user.getId());
         ApiResponse<Boolean> response = new ApiResponse<>(true, "", "", true);
         return ResponseEntity.ok(response);
     }
 
 
     @Operation(summary = "사용자 수정")
-    @PutMapping("/v1.0/user/{id}")
-    public ResponseEntity<ApiResponse<UserInfoDto>> updateUser(@PathVariable Long id, @RequestBody @Valid UserUpdateDto userUpdateDto) {
-        final Optional<Users> user = userService.findUserById(id);
-        if (user.isEmpty()) {
-            ApiResponse<UserInfoDto> errorResponse = new ApiResponse<>(false, "USER_NOT_FOUND", "UserId " + id + " not found", null);
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
-        }
-        userService.updateUser(id, userUpdateDto.getPassword(), userUpdateDto.getEmail());
-        final UserInfoDto userInfo = new UserInfoDto(user.get().getUserId(), userUpdateDto.getEmail().isEmpty() ? user.get().getEmail() : userUpdateDto.getEmail().get());
+    @PutMapping("/v1.0/user")
+    public ResponseEntity<ApiResponse<UserInfoDto>> updateUser(@RequestBody @Valid UserUpdateDto userUpdateDto) {
+        final Users user = authUtil.getUser();
+        userService.updateUser(user.getId(), userUpdateDto.getPassword(), userUpdateDto.getEmail());
+        final UserInfoDto userInfo = new UserInfoDto(user.getUserId(), userUpdateDto.getEmail().isEmpty() ? user.getEmail() : userUpdateDto.getEmail().get());
 
         ApiResponse<UserInfoDto> response = new ApiResponse<>(true, "", "", userInfo);
         return ResponseEntity.ok(response);
